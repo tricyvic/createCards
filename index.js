e08ab334a6f28d271027ef718ea2c6e129ba5008
Victor Muhita <victor.muhita@student.moringaschool.com>
2023-03-18 08:28:12 +0300

    add styles to card

---
+++ index.js
@@ -0,0 +1,13 @@
+//create an object that has differnte text and date
+//loop through the array by calling a function that injects the data
+
+//let titleArray = [{title:"the first title",paragraph:"jan 31 2023"},{title:"the second title",paragraph:"Mar 1 2023"},{title:"the third title",paragraph:"Feb 31 2023"}]
+let element = document.getElementById("display")
+element.addEventListener("click", function(){ alert("Hello World!"); });
+
+// const myFunction = () =>{
+//     alert("hi")
+// }
+
+
+
